aa691c946c6f41cf3d2302a7f5b0cda460d34dbf
Spezialk-dev <spezialk-dev@posteo.com>
2026-04-19 13:20:40 +0200

    moved from static dirs to xdg compliant dirs

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use std::io::Write;
 use std::{io::Read};
 use std::fs::OpenOptions;
 use notify_rust::{Notification, Hint};
-
-const BASE_PATH: &'static str =  "/var/lib/bootsecure/";
-const BASE_CONFIG: &'static str =  "/var/lib/bootsecure/save_mtc";
+use std::path::PathBuf;
 
 
 // all paarsing for monotonic counter
@@ -46,25 +44,23 @@ fn read_MTC(value :&mut u16) -> std::io::Result<()>{
 }
 //storing and and restoring the value to a config file
 //we want the value to be consumed when writing since its the last action being done
-fn write_value_to_config(data: String)-> std::io::Result<()>{
-    fs::create_dir_all(BASE_PATH)?;
-
+fn write_value_to_config(data: String, path_mtc: &PathBuf )-> std::io::Result<()>{
     let mut file = OpenOptions::new()
         .write(true)
         .create(true)
         .truncate(true)
-        .open(BASE_CONFIG)?;
+        .open(path_mtc)?;
     file.write_all(data.as_bytes())?;
     Ok(())
 }
 
-fn read_value_from_mtc_val(value :&mut u16)->std::io::Result<()>{
-    let exists = match fs::exists(BASE_CONFIG) {
+fn read_value_from_mtc_val(value :&mut u16, path_mtc: &PathBuf)->std::io::Result<()>{
+    let exists = match fs::exists(path_mtc) {
         Ok(val) => val,
         Err(_) => false // if something went wrong the file might as well not exist
     };
     if exists{
-        let content_mtc = match fs::read_to_string(BASE_CONFIG){
+        let content_mtc = match fs::read_to_string(path_mtc){
             Ok(val) => val,
             Err(err) => {print!("The following error occued while reading {err}");String::from("0")} //if 0 is returned from config file
         };
@@ -79,11 +75,15 @@ fn read_value_from_mtc_val(value :&mut u16)->std::io::Result<()>{
 }
 
 fn main() {
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("bootsecure");
+    let mtc_value = xdg_dirs.place_data_file("mtc_value")
+        .expect("cannot create bootsecure directory");
+
     let mut old_mtc : u16 = 0;
     let mut current_mtc : u16 = 0;
-    let _ = read_value_from_mtc_val(&mut old_mtc);
+    let _ = read_value_from_mtc_val(&mut old_mtc, &mtc_value);
     let _ = read_MTC(&mut current_mtc);
-    let _ =write_value_to_config(current_mtc.to_string());
+    let _ =write_value_to_config(current_mtc.to_string(), &mtc_value);
 
     // user ouput needs to be replaced with logging to journalctl and notification
     let notification_boody = format!("likely Harddrive transplantation due to missmatch of MTC values - (current MTC): {} (Saved MTC) {} ", &current_mtc.to_string() , &old_mtc.to_string());
